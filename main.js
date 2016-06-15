5e63d398a318a99bcfd8a5b67501001bb2ceb716
Peter Lee <peter.lee@tufts.edu>
2016-06-15 13:49:08 -0400

    data binding

--- main.js
+++ main.js
@@ -4,6 +4,10 @@ var jsdom = require("jsdom");
 var port = 8080;
 var document = jsdom.jsdom();
 
+var data = [{"country": "Angola", "equity": true, "bonds" : false, "credits" : false},
+	    {"country": "Bermuda", "equity": false, "bonds" : false, "credits": true},
+	    {"country": "China", "equity": true, "bonds" : true, "credits": true}]
+
 var server = http.createServer(function (req, res) {
 	var width = 960,
 	    height = 500;
@@ -15,10 +19,28 @@ var server = http.createServer(function (req, res) {
 	   .attr("cy", 200)
 	   .attr("r", 200)
 	   .style("fill", "brown")
-	   .style("fill-opacity", ".5");
+	   .style("fill-opacity", getOpacity(data, r => r.equity));
+	svg.append("circle")
+	   .attr("cx", 550)
+	   .attr("cy", 200)
+	   .attr("r", 200)
+	   .style("fill", "steelblue")
+	   .style("fill-opacity", getOpacity(data, r => r.bonds));
+	svg.append("circle")
+	   .attr("cx", 450)
+	   .attr("cy", 300)
+	   .attr("r", 200)
+	   .style("fill", "green")
+	   .style("fill-opacity", getOpacity(data, r => r.credits));
 	res.writeHead(200);
 	res.end(document.documentElement.outerHTML);
 });
+
+function getOpacity(rows, cmp) {
+	var sum = rows.reduce( (prev, curr) => cmp(curr) ? prev + 1 : prev, 0);
+	return sum / rows.length;
+}
+
 server.listen(port, function() {
 	console.log("Server listening on port " + port.toString());
 });

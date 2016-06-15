97d387bf9da6a18ccfd246ae087a813aeaffd4cb
Peter Lee <peter.lee@tufts.edu>
2016-06-15 13:36:16 -0400

    d3 venn diagram hello world

--- main.js
+++ main.js
@@ -5,11 +5,19 @@ var port = 8080;
 var document = jsdom.jsdom();
 
 var server = http.createServer(function (req, res) {
-	var vis = d3.select(document.body).html('').append('svg')
-		.attr('width', 500)
-		.attr('height', 500);
+	var width = 960,
+	    height = 500;
+	var svg = d3.select(document.body).html('').append("svg")
+		.attr("width", width)
+		.attr("height", height);
+	svg.append("circle")
+	   .attr("cx", 350)
+	   .attr("cy", 200)
+	   .attr("r", 200)
+	   .style("fill", "brown")
+	   .style("fill-opacity", ".5");
 	res.writeHead(200);
-	res.end(document.body.html);
+	res.end(document.documentElement.outerHTML);
 });
 server.listen(port, function() {
 	console.log("Server listening on port " + port.toString());

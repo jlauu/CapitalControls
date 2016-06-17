53189081ba5ee1d2de7b957998ef761525aef5d4
Peter Lee <peter.lee@tufts.edu>
2016-06-17 12:20:11 -0400

    total counts and rings

--- public/main.js
+++ public/main.js
@@ -1,21 +1,22 @@
 var categories = {bonds : 0, equity: 1, credits : 2};
-var svg, venn, data;
+var svg, venn, data, year;
 
 // all combinations of asset categories
-var flags = (function () {
-    var flags = [];
+var subsets = (function () {
+    var set = [];
     for (var i = 0; i < 2; i++) {
         for (var j = 0; j < 2; j++) {
             for (var k = 0; k < 2; k++) {
                 if (i == 0 && j == 0 && k == 0) continue;
-                flags.push({bonds : i, equity : j, credits : k});
+                set.push({bonds : i, equity : j, credits : k});
             }
          }
     }
-    return flags;
+    return set;
 })();
 
-function update(year) {
+function update(y) {
+    year = y;
     var xhr = new XMLHttpRequest();
     xhr.onreadystatechange = function () {
         if (xhr.readyState == 4 && xhr.status == 200) {
@@ -30,21 +31,24 @@ function update(year) {
 
 function updateTextBox(x, y) { 
     d3.selectAll(".country-text").remove();
-    flags.forEach(function (flag) {
-        var inBounds = makeInBounds(flag, venn);
+    subsets.forEach(function (set) {
+        var inBounds = makeInBounds(set, venn);
         if (inBounds(x, y)) {
             var textbox = d3.select("#infobox")
             var countries = data.filter(function (row) {
-                return flags.bonds == row.bonds &&
-                       flags.equity == row.equity &&
-                       flags.credits == row.credits;
+                return set.bonds == row.bonds &&
+                       set.equity == row.equity &&
+                       set.credits == row.credits;
             });
             var list = textbox.append("ul")
                         .attr("class", "country-text")
                         .attr("id", "countries");
             // Title
-            var Assets = Object.keys(flags)
-                            .filter(function (k) {return flags[k]});
+            list.append("li")
+                .text("Year: " + year.toString())
+                .attr("class", "country-text");
+            var Assets = Object.keys(set)
+                            .filter(function (k) {return set[k]});
             list.append("li")
                 .text("Assets: " + Assets)
                 .attr("class", "country-text");
@@ -81,6 +85,7 @@ function init() {
     venn.circles.forEach(function (circle) {
         svg.append("circle")
            .attr("class", "venn-circle")
+           .attr("id", i)
            .attr("cx", circle.cx)
            .attr("cy", circle.cy)
            .attr("r", circle.r)
@@ -88,12 +93,49 @@ function init() {
            .style("fill", "none")
         i += 1;
     });
-    update(2013);
+    update(1995);
 }
 
 function populate(data) {
     var dots = svg.selectAll(".dot").remove();
     if (!data) return;
+
+    subsets.forEach(function (set) {
+        var circles = Object.keys(set)
+                .filter(function (k) {return set[k];})
+                .map(function(k) {return venn.circles[categories[k]];});
+        var midpoint = ['cx','cy']
+                .map(function (property) {
+                    return circles.map(function (c) {return c[property];})
+                            .reduce(function (a,b) {return a + b;}) / circles.length;
+                });
+        var fdata = data.filter(function (row) {
+            return row.equity == set.equity &&
+                   row.bonds == set.bonds &&
+                   row.credits == set.credits;
+        });
+        // Ring symbol
+        var n = fdata.length;
+        var apothem = .2 * venn.circles[0].r;
+        for (var i=0; i<n; i++) {
+            var dot_r = apothem * Math.tan(Math.PI / n);
+            svg.append("circle")
+               .attr("class", "dot")
+               .attr("cx", n==1 ? midpoint[0] : apothem*Math.cos(i*2*Math.PI/n) + midpoint[0])
+               .attr("cy", n==1 ? midpoint[1] : apothem*Math.sin(i*2*Math.PI/n) + midpoint[1])
+               .attr("r", n<3 ? n*.5 * apothem : dot_r/2)
+               .attr("stroke", "black")
+               .style("fill", getPointColor(set));
+        }
+        // Number count
+        svg.append("text")
+           .attr("class", "dot")
+           .attr("x", midpoint[0])
+           .attr("y", midpoint[1])
+           .attr("text-anchor", "middle")
+           .text(fdata.length.toString());
+    });
+    /* Randomly drawing individual dots
     data.forEach(function (row) {
         if (!(row.bonds || row.equity || row.credits)) return;
         var pt = randomPoint(makePlaceFunc(row, venn), makeInBounds(row, venn));
@@ -104,6 +146,7 @@ function populate(data) {
            .attr("r", 3)
            .style("fill", getPointColor(row));
     });
+    */
 }
 
 function getOpacity(rows, cmp) {
@@ -114,7 +157,7 @@ function getOpacity(rows, cmp) {
 }
 
 function getPointColor(row) {
-    return d3.hsl(360 * ((row.bonds + row.equity + row.credits) / 3), .8, .5);
+    return d3.hsl(75 + 220 * ((row.bonds + row.equity + row.credits) / 3), .8, .5);
 }
 
 function makeInBounds(row, venn) {
@@ -134,9 +177,7 @@ function makePlaceFunc(row, venn) {
         category = categories.equity;
     } else if (row.credits) {
         category = categories.credits;
-    } else {
-        console.log("no cats");
-    }
+    } 
     var circle = venn.circles[category];
     var f = function() {
         return [Math.random() * circle.r * Math.cos(Math.random() * Math.PI * 2) + circle.cx,
